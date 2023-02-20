cd50d830dc965cd7daf0859eea48f4b4bf921195
mooreant <mooreant@iu.edu>
2023-02-19 21:51:32 -0500

    fixed id route

--- api/routes/api/v1/index.js
+++ api/routes/api/v1/index.js
@@ -22,8 +22,10 @@ router.get('/campaigns', async (req, res) => {
 router.get('/campaigns/:id', async (req, res) => {
   const filter = {_id: req.params.id};
   let campaign = await Campaign.findOne(filter);
-  let donations = Donation.find({campaign_id: req.params.id});
-  campaign.donations = donations;
+  if (campaign) {
+    let donations = Donation.find({campaign_id: req.params.id});
+    campaign.donations = donations;
+  }
   res.json(campaign);
 });
 
